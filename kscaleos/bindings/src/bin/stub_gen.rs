424a3b94ba737b1286c03cce1ebee47753a71188
Benjamin Bolte <ben@bolte.cc>
2024-10-27 22:36:52 -0700

    stuff

--- kscaleos/bindings/src/bin/stub_gen.rs
+++ kscaleos/bindings/src/bin/stub_gen.rs
@@ -1,7 +1,9 @@
 use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
+    println!("Generating stub info...");
     let stub = bindings::stub_info()?;
+    println!("Generating stubs...");
     stub.generate()?;
     Ok(())
 }
--- kscaleos/bindings/src/lib.rs
+++ kscaleos/bindings/src/lib.rs
@@ -1,10 +1,18 @@
 use kscaleos::hello_world as kscaleos_hello_world;
 use pyo3::prelude::*;
+use pyo3::{wrap_pyfunction, PyResult};
 use pyo3_stub_gen::define_stub_info_gatherer;
 
 #[pyfunction]
-pub fn hello_world() {
+pub fn hello_world() -> PyResult<()> {
     kscaleos_hello_world();
+    Ok(())
+}
+
+#[pymodule]
+fn bindings(m: &Bound<PyModule>) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(hello_world))?;
+    Ok(())
 }
 
 define_stub_info_gatherer!(stub_info);
