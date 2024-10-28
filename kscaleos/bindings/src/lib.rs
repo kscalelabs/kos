dc12d03180922b03cee03be34e9b57b69b550839
Benjamin Bolte <ben@bolte.cc>
2024-10-27 22:29:57 -0700

    stub gen

--- kscaleos/bindings/src/lib.rs
+++ kscaleos/bindings/src/lib.rs
@@ -1,5 +1,10 @@
 use kscaleos::hello_world as kscaleos_hello_world;
+use pyo3::prelude::*;
+use pyo3_stub_gen::define_stub_info_gatherer;
 
+#[pyfunction]
 pub fn hello_world() {
     kscaleos_hello_world();
 }
+
+define_stub_info_gatherer!(stub_info);
