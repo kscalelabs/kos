87e49cbf7a100035db4c8fe28c44ee3f7eb9821b
Denys Bezmenov <me@hatomist.pw>
2024-11-11 11:42:27 -0800

    Added protobuf imports in rust

--- core/build.rs
+++ core/build.rs
@@ -3,31 +3,23 @@ use std::path::PathBuf;
 
 fn main() {
     // Path to the Protobuf files
-    let proto_root = "../protos";
+    let proto_root = "../proto";
 
     // Where to output the compiled Rust files
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     // List of Protobuf files
     let protos = [
-        "common.proto",
-        "actuator.proto",
-        "imu.proto",
-        "inference.proto",
-        "process_manager.proto",
-        "system.proto",
+        "kos/common.proto",
+        "kos/actuator.proto",
+        "kos/imu.proto",
+        "kos/inference.proto",
+        "kos/process_manager.proto",
+        "kos/system.proto",
+        "google/longrunning/operations.proto",
     ];
 
-    // Full paths for protos
-    let proto_files: Vec<String> = protos
-        .iter()
-        .map(|p| format!("{}/kos/{}", proto_root, p))
-        .collect();
-
     let includes = [proto_root, &format!("{}/googleapis", proto_root)];
-
-    println!("proto_files: {:?}", proto_files);
-    println!("includes: {:?}", includes);
 
     // Create the output directory
     std::fs::create_dir_all(out_dir.join("kos"))
@@ -37,7 +29,7 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .out_dir(out_dir.join("kos"))
-        .compile_protos(&proto_files, &includes)
+        .compile_protos(&protos, &includes)
         .expect("Failed to compile protos");
 
     // Re-run the build script if any of the proto files change
