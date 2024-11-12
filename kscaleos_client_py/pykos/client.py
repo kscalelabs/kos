84ba3a7281e95161af10cc6e6f383855e8a29c52
Denys Bezmenov <me@hatomist.pw>
2024-11-11 17:55:54 -0800

    stub imu impl, python & rust grpc

---
+++ kscaleos_client_py/pykos/client.py
@@ -0,0 +1,26 @@
+import grpc
+
+from .services.imu import IMUServiceClient
+
+class KOS:
+    """
+    KOS client
+
+    Args:
+        ip (str, optional): IP address of the robot running KOS. Defaults to localhost.
+        port (int, optional): Port of the robot running KOS. Defaults to 50051.
+
+    Attributes:
+        imu (IMUServiceClient): Client for the IMU service.
+    """
+    def __init__(self, ip: str = "localhost", port: int = 50051):
+        self.ip = ip
+        self.port = port
+        self.channel = grpc.insecure_channel(f"{self.ip}:{self.port}")
+        self.imu = IMUServiceClient(self.channel)
+
+    def close(self):
+        """
+        Close the gRPC channel.
+        """
+        self.channel.close()
--- kscaleos_client_py/setup.py
+++ kscaleos_client_py/setup.py
@@ -3,58 +3,43 @@
 """Setup script for the project."""
 
 import re
-import subprocess
 
-from setuptools import find_packages, setup
-from setuptools.command.build_ext import build_ext
-from setuptools_rust import Binding, RustExtension
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description: str = f.read()
 
 
-with open("kscaleos/requirements.txt", "r", encoding="utf-8") as f:
+with open("pykos/requirements.txt", "r", encoding="utf-8") as f:
     requirements: list[str] = f.read().splitlines()
 
 
-with open("kscaleos/requirements-dev.txt", "r", encoding="utf-8") as f:
+with open("pykos/requirements-dev.txt", "r", encoding="utf-8") as f:
     requirements_dev: list[str] = f.read().splitlines()
 
 
-with open("kscaleos/__init__.py", "r", encoding="utf-8") as fh:
+with open("pykos/__init__.py", "r", encoding="utf-8") as fh:
     version_re = re.search(r"^__version__ = \"([^\"]*)\"", fh.read(), re.MULTILINE)
-assert version_re is not None, "Could not find version in kscaleos/__init__.py"
+assert version_re is not None, "Could not find version in pykos/__init__.py"
 version: str = version_re.group(1)
 
 
-class RustBuildExt(build_ext):
-    def run(self) -> None:
-        # Run the stub generator
-        subprocess.run(["cargo", "run", "--bin", "stub_gen"], check=True)
-        # Call the original build_ext command
-        super().run()
-
-
 setup(
-    name="kscaleos",
+    name="pykos",
     version=version,
-    description="The K-Scale Operating System",
-    author="Benjamin Bolte",
-    url="https://github.com/kscalelabs/kscaleos",
-    rust_extensions=[
-        RustExtension(
-            target="kscaleos.bindings",
-            path="kscaleos/bindings/Cargo.toml",
-            binding=Binding.PyO3,
-        ),
-    ],
-    setup_requires=["setuptools-rust"],
+    description="The KOS command line interface",
+    author="pykos Contributors",
+    url="https://github.com/kscalelabs/kos",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.11",
+    python_requires=">=3.8",
     install_requires=requirements,
     tests_require=requirements_dev,
     extras_require={"dev": requirements_dev},
-    packages=find_packages(include=["kscaleos"]),
-    cmdclass={"build_ext": RustBuildExt},
+    packages=["pykos"],
+    entry_points={
+        "console_scripts": [
+            "pykos=pykos.cli:cli",
+        ],
+    },
 )
