896276711f169404a9f6b761f873904de0a9382a
budzianowski <budzianowski@gmail.com>
2024-11-24 00:28:04 +0000

    fix krec protos, add left arm config temp

--- pykos/pykos/services/process_manager.py
+++ pykos/pykos/services/process_manager.py
@@ -1,18 +1,19 @@
-"Process manager service client"
+"""Process manager service client"""
+from typing import Optional, Tuple
 
 import grpc
 from google.protobuf.empty_pb2 import Empty
-from typing import Optional, Tuple
 
-from kos import process_manager_pb2, process_manager_pb2_grpc
+from kos import process_manager_pb2_grpc
 from kos.common_pb2 import Error
+from kos.process_manager_pb2 import KClipStartRequest
 
 
 class ProcessManagerServiceClient:
     def __init__(self, channel: grpc.Channel) -> None:
         self.stub = process_manager_pb2_grpc.ProcessManagerServiceStub(channel)
 
-    def start_kclip(self) -> Tuple[Optional[str], Optional[Error]]:
+    def start_kclip(self, request: KClipStartRequest) -> Tuple[Optional[str], Optional[Error]]:
         """Start KClip recording.
 
         Returns:
@@ -20,10 +21,10 @@ class ProcessManagerServiceClient:
             - clip_uuid (str): UUID of the started clip, if successful
             - error (Error): Error details if the operation failed
         """
-        response = self.stub.StartKClip(Empty())
+        response = self.stub.StartKClip(request)
         return response.clip_uuid, response.error if response.HasField("error") else None
 
-    def stop_kclip(self) -> Tuple[Optional[str], Optional[Error]]:
+    def stop_kclip(self, request: Empty = Empty()) -> Tuple[Optional[str], Optional[Error]]:
         """Stop KClip recording.
 
         Returns:
@@ -31,6 +32,5 @@ class ProcessManagerServiceClient:
             - clip_uuid (str): UUID of the stopped clip, if successful
             - error (Error): Error details if the operation failed
         """
-        response = self.stub.StopKClip(Empty())
+        response = self.stub.StopKClip(request)
         return response.clip_uuid, response.error if response.HasField("error") else None
-
