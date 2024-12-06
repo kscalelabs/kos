0d33ba2783cfe0a98da5380648544ff88e2375f1
WT-MM <wesley.maa@gmail.com>
2024-12-06 02:55:54 +0000

    compression

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -14,14 +14,17 @@ use tokio::sync::Mutex;
 use tonic::transport::Server;
 use tracing::{debug, error, info};
 use tracing_subscriber::filter::EnvFilter;
-use tracing_appender::rolling::RollingFileAppender;
-use tracing_appender::non_blocking::NonBlocking;
 use tracing_subscriber::Layer;
 use tracing_subscriber::prelude::*;
 use chrono::Local;
 use clap::Parser;
 use directories::BaseDirs;
 use std::path::PathBuf;
+use std::io::{self, Write, BufWriter};
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+use tokio::signal;
 
 #[cfg(not(any(feature = "kos-sim", feature = "kos-zeroth-01", feature = "kos-kbot")))]
 use kos_stub::StubPlatform as PlatformImpl;
@@ -84,6 +87,81 @@ struct DaemonState {
     platform: PlatformImpl,
 }
 
+struct CompressedWriter {
+    encoder: Option<GzEncoder<BufWriter<File>>>,
+    path: PathBuf,
+}
+
+impl CompressedWriter {
+    fn new(path: impl AsRef<std::path::Path>) -> io::Result<Self> {
+        let file = File::create(path.as_ref())?;
+        let buffered = BufWriter::new(file);
+        Ok(Self {
+            encoder: Some(GzEncoder::new(buffered, Compression::new(6))),
+            path: path.as_ref().to_path_buf(),
+        })
+    }
+
+    fn sync(&mut self) -> io::Result<()> {
+        if let Some(encoder) = &mut self.encoder {
+            encoder.flush()?;
+            let buf_writer = encoder.get_mut();
+            buf_writer.flush()?;
+            let file = buf_writer.get_mut();
+            file.sync_all()?;
+        }
+        Ok(())
+    }
+
+    fn finalize(&mut self) -> io::Result<()> {
+        info!("Finalizing compressed log {}", self.path.display());
+        if let Some(encoder) = self.encoder.take() {
+            // Finish the compression
+            let mut buf_writer = encoder.finish()?;
+            // Flush the buffer
+            buf_writer.flush()?;
+            info!("Flushed compressed log {}", self.path.display());
+            // Sync to disk
+            buf_writer.get_mut().sync_all()?;
+        }
+        Ok(())
+    }
+}
+
+impl Write for CompressedWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        if let Some(encoder) = &mut self.encoder {
+            match encoder.write(buf) {
+                Ok(size) => {
+                    if size > 0 && buf.contains(&b'\n') {
+                        self.sync()?;
+                    }
+                    Ok(size)
+                }
+                Err(e) => {
+                    error!("Failed to write to compressed log {}: {}", self.path.display(), e);
+                    Err(e)
+                }
+            }
+        } else {
+            error!("Attempted to write to finalized log {}", self.path.display());
+            Err(io::Error::new(io::ErrorKind::Other, "Writer has been finalized"))
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.sync()
+    }
+}
+
+impl Drop for CompressedWriter {
+    fn drop(&mut self) {
+        if let Err(e) = self.finalize() {
+            error!("Failed to finalize compressed log {}: {}", self.path.display(), e);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
@@ -119,16 +197,20 @@ async fn main() -> Result<()> {
         std::fs::create_dir_all(&log_dir)?;
 
         let timestamp = Local::now().format("%Y%m%d_%H%M%S");
-        let log_name = format!("kos-daemon_{}.log", timestamp);
+        let temp_name = format!("kos-daemon_{}.log", timestamp);
+        let final_name = format!("kos-daemon_{}.log.gz", timestamp);
+        let log_path = log_dir.join(&final_name);
 
-        let file_appender = RollingFileAppender::new(
-            tracing_appender::rolling::Rotation::NEVER,
-            log_dir,
-            &log_name,
-        );
-        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+        info!("Writing compressed logs to: {}", log_path.display());
+        
+        let compressed_writer = CompressedWriter::new(&log_path)?;
+        let (non_blocking, guard) = tracing_appender::non_blocking(compressed_writer);
 
         let file_layer = tracing_subscriber::fmt::layer()
+            .with_thread_ids(true)
+            .with_target(true)
+            .with_file(true)
+            .with_line_number(true)
             .with_writer(non_blocking)
             .with_filter(tracing_subscriber::filter::LevelFilter::TRACE);
 
@@ -144,6 +226,15 @@ async fn main() -> Result<()> {
         platform: PlatformImpl::new(),
     };
 
+    // Setup signal handler
+    let (shutdown_tx, mut shutdown_rx) = tokio::sync::oneshot::channel();
+    
+    tokio::spawn(async move {
+        if let Ok(()) = signal::ctrl_c().await {
+            let _ = shutdown_tx.send(());
+        }
+    });
+
     // telemetry
     Telemetry::initialize(
         format!("{}-{}", state.platform.name(), state.platform.serial()).as_str(),
@@ -157,9 +248,16 @@ async fn main() -> Result<()> {
 
     state.platform.initialize(operations_service.clone())?;
 
-    if let Err(e) = run_server(&state.platform, operations_service).await {
-        error!("Server error: {:?}", e);
-        std::process::exit(1);
+    tokio::select! {
+        res = run_server(&state.platform, operations_service) => {
+            if let Err(e) = res {
+                error!("Server error: {:?}", e);
+                std::process::exit(1);
+            }
+        }
+        _ = shutdown_rx => {
+            info!("Received shutdown signal, cleaning up...");
+        }
     }
 
     Ok(())
