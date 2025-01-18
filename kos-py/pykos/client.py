a215fbf1390935997770588ff6651316b342d382
Denys Bezmenov <me@hatomist.pw>
2025-01-17 18:07:25 -0800

    led matrix / sound protos (#39)

--- kos-py/pykos/client.py
+++ kos-py/pykos/client.py
@@ -5,8 +5,10 @@ import grpc
 from pykos.services.actuator import ActuatorServiceClient
 from pykos.services.imu import IMUServiceClient
 from pykos.services.inference import InferenceServiceClient
+from pykos.services.led_matrix import LEDMatrixServiceClient
 from pykos.services.process_manager import ProcessManagerServiceClient
 from pykos.services.sim import SimServiceClient
+from pykos.services.sound import SoundServiceClient
 
 
 class KOS:
@@ -26,6 +28,8 @@ class KOS:
         self.channel = grpc.insecure_channel(f"{self.ip}:{self.port}")
         self.imu = IMUServiceClient(self.channel)
         self.actuator = ActuatorServiceClient(self.channel)
+        self.led_matrix = LEDMatrixServiceClient(self.channel)
+        self.sound = SoundServiceClient(self.channel)
         self.process_manager = ProcessManagerServiceClient(self.channel)
         self.inference = InferenceServiceClient(self.channel)
         self.sim = SimServiceClient(self.channel)
