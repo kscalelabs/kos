aa7b709656a4811db6acc9d33bdf51cfcb9cf923
Wesley Maa <wesley.maa@gmail.com>
2025-04-03 19:52:54 -0700

    add markers to sim service

--- kos-py/pykos/services/sim.py
+++ kos-py/pykos/services/sim.py
@@ -1,6 +1,7 @@
 """Sim service client."""
 
-from typing import NotRequired, TypedDict, Unpack
+from dataclasses import dataclass
+from typing import Literal, NotRequired, TypedDict, Unpack
 
 import grpc
 import grpc.aio
@@ -44,6 +45,31 @@ class SimulationParameters(TypedDict):
     time_scale: NotRequired[float]
     gravity: NotRequired[float]
 
+@dataclass
+class MarkerRGBA():
+    r: float
+    g: float
+    b: float
+    a: float
+
+class Marker(TypedDict):
+    name: str
+    marker_type: Literal["sphere", "box", "capsule", "cylinder", "arrow"]
+    target_name: str
+    target_type: Literal["geom", "body"]
+    scale: list[float]
+    offset: NotRequired[list[float]]
+    color: NotRequired[MarkerRGBA]
+    label: NotRequired[bool]
+    track_rotation: NotRequired[bool]
+
+class UpdateMarkerRequest(TypedDict):
+    name: str
+    marker_type: NotRequired[Literal["sphere", "box", "capsule", "cylinder", "arrow"]]
+    offset: NotRequired[list[float]]
+    color: NotRequired[MarkerRGBA]
+    scale: NotRequired[list[float]]
+    label: NotRequired[bool]
 
 class SimServiceClient(AsyncClientBase):
     """Client for the SimulationService."""
@@ -52,6 +78,113 @@ class SimServiceClient(AsyncClientBase):
         super().__init__()
 
         self.stub = sim_pb2_grpc.SimulationServiceStub(channel)
+
+    async def add_marker(self, **kwargs: Unpack[Marker]) -> common_pb2.ActionResponse:
+        """Add a marker to the simulation.
+
+        Args:
+            **kwargs: Marker parameters
+            name: Name of the marker
+            type: Type of marker
+            track_name: Name of the track to attach the marker to
+            track_type: Type of track to attach the marker to
+            scale: Scale of the marker
+            offset: (optional) Offset of the marker
+            color: (optional) Color of the marker
+            label: (optional) Whether to show the marker label
+            track_rotation: (optional) Whether to track the marker rotation
+
+        Returns:
+            ActionResponse indicating success/failure
+        """
+        if offset_list := kwargs.get("offset"):
+            offset = sim_pb2.Marker.Offset(x=offset_list[0], y=offset_list[1], z=offset_list[2])
+        else:
+            offset = sim_pb2.Marker.Offset(x=0.0, y=0.0, z=0.0)
+
+        if color_dataclass := kwargs.get("color"):
+            color = sim_pb2.Marker.RGBA(r=color_dataclass.r, g=color_dataclass.g, b=color_dataclass.b, a=color_dataclass.a)
+        else:
+            color = sim_pb2.Marker.RGBA(r=1.0, g=0.0, b=0.0, a=1.0)
+        request = sim_pb2.Marker(
+            name=kwargs["name"],
+            marker_type=getattr(sim_pb2.Marker.MarkerType, str.upper(kwargs["marker_type"])),
+            target_name=kwargs["target_name"],
+            target_type=getattr(sim_pb2.Marker.TargetType, str.upper(kwargs["target_type"])),
+            scale=sim_pb2.Marker.Scale(scale=kwargs["scale"]),
+            color=color,
+            label=kwargs.get("label", False),
+            track_rotation=kwargs.get("track_rotation", False),
+            offset=offset,
+        )
+
+        return await self.stub.AddMarker(request)
+
+    async def update_marker(self, name: str, **kwargs: Unpack[UpdateMarkerRequest]) -> common_pb2.ActionResponse:
+        """Update a marker in the simulation.
+
+        Args:
+            name: Name of the marker to update
+            **kwargs: Marker parameters
+            type: (optional) Type of marker
+            offset: (optional) Offset of the marker
+            color: (optional) Color of the marker
+            label: (optional) Whether to show the marker label
+            track_rotation: (optional) Whether to track the marker rotation
+
+        Returns:
+            ActionResponse indicating success/failure
+        """
+        if type_str := kwargs.get("marker_type"):
+            marker_type = getattr(sim_pb2.Marker.MarkerType, str.upper(type_str))
+        else:
+            marker_type = None
+
+        if offset_list := kwargs.get("offset"):
+            offset = sim_pb2.Marker.Offset(x=offset_list[0], y=offset_list[1], z=offset_list[2])
+        else:
+            offset = None
+
+        if color_dataclass := kwargs.get("color"):
+            color = sim_pb2.Marker.RGBA(r=color_dataclass.r, g=color_dataclass.g, b=color_dataclass.b, a=color_dataclass.a)
+        else:
+            color = None
+
+        if scale_list := kwargs.get("scale"):
+            scale = sim_pb2.Marker.Scale(scale=scale_list)
+        else:
+            scale = None
+
+        request = sim_pb2.UpdateMarkerRequest(
+            name=name,
+            marker_type=marker_type,
+            offset=offset,
+            color=color,
+            label=kwargs.get("label"),
+            scale=scale,
+        )
+
+        return await self.stub.UpdateMarker(request)
+
+    async def remove_marker(self, name: str) -> common_pb2.ActionResponse:
+        """Remove a marker from the simulation.
+
+        Args:
+            name: Name of the marker to remove
+
+        Returns:
+            ActionResponse indicating success/failure
+        """
+        request = sim_pb2.RemoveMarkerRequest(name=name)
+        return await self.stub.RemoveMarker(request)
+
+    async def get_markers(self) -> sim_pb2.GetMarkersResponse:
+        """Get all markers in the simulation.
+
+        Returns:
+            GetMarkersResponse containing all markers
+        """
+        return await self.stub.GetMarkers(Empty())
 
     async def reset(self, **kwargs: Unpack[ResetRequest]) -> common_pb2.ActionResponse:
         """Reset the simulation to its initial state.
